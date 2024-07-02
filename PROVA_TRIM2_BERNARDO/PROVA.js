48e82089c1928c9595fb57b51f4b1bf40b88c190
berna1704 <129773407+berna1704@users.noreply.github.com>
2024-07-02 09:05:13 -0300

    'first commit"

---
+++ PROVA_TRIM2_BERNARDO/PROVA.js
@@ -0,0 +1,11 @@
+console.log("Mês de nascimento");
+
+console.log("Janeiro, Fevereiro, Março");
+
+let meses = ["Janeiro", "Fevereiro", "Março", "Abril"];
+
+for ( let mes of meses) {
+    if(mes =="Abril") {
+        console.log('Nasci em', mes)
+    }
+}
